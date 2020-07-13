24087bfadbb3648a54b5928d6e2b83b42b8238d8
Parth Gupta <parth1716@gmail.com>
2020-07-13 21:04:05 +0530

    Updates

---
+++ precache-manifest.e7398f737b96b169db32bfbbcb8ee744.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c29e13c2cde6d30367dafbde95346eef",
+    "url": "/weather-up/index.html"
+  },
+  {
+    "revision": "05f8d17b29b88f929899",
+    "url": "/weather-up/static/css/main.7e1861dd.chunk.css"
+  },
+  {
+    "revision": "8412d11fa9ee5d914f98",
+    "url": "/weather-up/static/js/2.8e1a3d53.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-up/static/js/2.8e1a3d53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05f8d17b29b88f929899",
+    "url": "/weather-up/static/js/main.2eb3e811.chunk.js"
+  },
+  {
+    "revision": "1c7ef46b65b9eddfe573",
+    "url": "/weather-up/static/js/runtime-main.f13df66a.js"
+  },
+  {
+    "revision": "c0e3733ce65a33e21063644e90426e1d",
+    "url": "/weather-up/static/media/cold-weather.c0e3733c.jpg"
+  },
+  {
+    "revision": "d162c77202ce64956ba7694573f99d72",
+    "url": "/weather-up/static/media/hot-weather.d162c772.jpg"
+  }
+]);
